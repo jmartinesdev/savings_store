d1d48d827d041b85f3368382a3557dc7a5407535
Jakeline Martines <jakeline_13_martines@hotmail.com>
2024-01-21 03:57:11 +0000

    create files about us and contact us, also import all the home, contact and about us page to urls patterns

--- savings_store/views.py
+++ savings_store/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Home Page",
+        "content": "Home Page"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "About us",
+        "content": "About us"
+    }
+    return render(request, "about/view.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Contact Us",
+        "content": "Contact Page"
+    }
+    return render(request, "contact/view.html", context)
