3583ca9849b6a959c63e50dc29ba90a34c3f9da3
Jakeline Martines <jakeline_13_martines@hotmail.com>
2024-01-22 00:06:26 +0000

    Implement Authentication Features and Update Forms and Views

--- savings_store/forms.py
+++ savings_store/forms.py
@@ -1,4 +1,5 @@
-from django import forms 
+from django import forms
+from django.contrib.auth import get_user_model
 
 class ContactForm(forms.Form):
     fullname = forms.CharField(
@@ -27,3 +28,35 @@ class ContactForm(forms.Form):
                 }
             )
     )
+
+class LoginForm(forms.Form):
+    username = forms.CharField(),
+    password = forms.CharField(widget=forms.PasswordInput)
+    
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username),
+        if qs.exists():
+            raise forms.ValidationError('This user already exist. Tap another name.')
+        return username
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email),
+        if qs.exists():
+            raise forms.ValidationError('This e-mail already exist. Try another one.')
+        return email
+    
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password != password2:
+            raise forms.ValidationError('Password do not match')
+        return data
