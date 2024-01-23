7b2739c703d6c20b08fc55d6a4023c6503660a5c
Jakeline Martines <jakeline_13_martines@hotmail.com>
2024-01-23 03:59:11 +0000

    Update product list view and template rendering

--- products/views.py
+++ products/views.py
@@ -1,20 +1,19 @@
-from django.views.generic import ListView
 from django.shortcuts import render
-
+from django.views.generic import ListView
 from .models import Product
 
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/products_list.html"
-    
-def get_context_data(self, *args, **kwargs):
-    context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    print(context)
-    return context
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).          get_context_data(*args, **kwargs)
+        print(context)
+        return context
 
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
-        'qs' : queryset
+        'object_list': queryset
     }
-    return render(request, "products/list.html", context)
+    return render(request, "products/products_list.html", context)
