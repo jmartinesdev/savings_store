a3d1815b5d49fb99a25a0bb81b9a390fa08dc8be
Jakeline Martines <jakeline_13_martines@hotmail.com>
2024-01-23 03:28:35 +0000

    created a folder templates/products and file products_list html

--- products/views.py
+++ products/views.py
@@ -5,6 +5,12 @@ from .models import Product
 
 class ProductListView(ListView):
     queryset = Product.objects.all()
+    template_name = "products/products_list.html"
+    
+def get_context_data(self, *args, **kwargs):
+    context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    print(context)
+    return context
 
 def product_list_view(request):
     queryset = Product.objects.all()
