78cd3718987e8ccf4245d5697c60d69eb177e40c
Jakeline Martines <jakeline_13_martines@hotmail.com>
2024-01-29 00:04:08 +0000

    created details views

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 class ProductListView(ListView):
@@ -17,3 +17,19 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/products_list.html", context)
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+    
+def ProductDetailView(request)
+    query_set = Product.objects.all()
+    context = {
+        'object_list': query_set
+    }
+    return render(request, "products/detail.html", context)
