f2ca1bd360c67a999f6afc94ec97700524e0e130
Jakeline Martines <jakeline_13_martines@hotmail.com>
2024-02-04 02:42:13 +0000

    file procfile created

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn savings_store_wsgi:app
--- savings_store/urls.py
+++ savings_store/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view, ProductFeaturedListView, ProductFeaturedDetailView
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -11,6 +11,8 @@ urlpatterns = [
 	path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('featured/', ProductFeaturedListView.as_view()),
+    path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
 	path('admin/', admin.site.urls),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
