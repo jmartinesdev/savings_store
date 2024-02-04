22adcc28fc6daf49fee335bb7d74ce2c74f1f32d
Jakeline Martines <jakeline_13_martines@hotmail.com>
2024-02-04 03:16:04 +0000

    update setting roots to deploy

--- savings_store/settings.py
+++ savings_store/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_local")]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 
